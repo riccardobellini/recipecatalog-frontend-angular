8a518c0314351f0311de5f58099c4c2007030ce1
Riccardo Bellini <riccardo.bellini1988@gmail.com>
2017-01-06 11:31:07 +0100

    Refactor: rename application main component and module

---
+++ src/app/recipe-catalog.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'recipe-catalog-root',
+  templateUrl: './recipe-catalog.component.html',
+  styleUrls: ['./recipe-catalog.component.scss']
+})
+export class RecipeCatalogComponent {
+  title = 'Hello World!';
+}
--- src/app/recipe-catalog.module.ts
+++ src/app/recipe-catalog.module.ts
@@ -3,11 +3,11 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
-import { AppComponent } from './app.component';
+import { RecipeCatalogComponent } from './recipe-catalog.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    RecipeCatalogComponent
   ],
   imports: [
     BrowserModule,
@@ -15,6 +15,6 @@ import { AppComponent } from './app.component';
     HttpModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [RecipeCatalogComponent]
 })
-export class AppModule { }
+export class RecipeCatalogModule { }
