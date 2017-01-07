127cff6c267c989cbf4b3893b1e48564903461e6
Riccardo Bellini <riccardo.bellini1988@gmail.com>
2017-01-07 16:13:14 +0100

    Basic dish type list structure

--- src/app/dish-type-list/dish-type-list.component.ts
+++ src/app/dish-type-list/dish-type-list.component.ts
@@ -1,11 +1,31 @@
 import { Component, OnInit } from '@angular/core';
 
+export interface DishType {
+  id: number;
+  name: string;
+}
+
 @Component({
   selector: 'rc-dish-type-list',
   templateUrl: './dish-type-list.component.html',
   styleUrls: ['./dish-type-list.component.scss']
 })
 export class DishTypeListComponent implements OnInit {
+
+  dishTypes: Array<DishType> = [
+    {
+      id: 1,
+      name: 'Antipasti'
+    },
+    {
+      id: 2,
+      name: 'Primi Piatti - Riso'
+    },
+    {
+      id: 3,
+      name: 'Secondi Piatti - Carne'
+    }
+  ];
 
   constructor() { }
 
