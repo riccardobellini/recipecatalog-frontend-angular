a531e418165be9a7cc88d3119b99ec09efc0750d
Riccardo Bellini <riccardo.bellini1988@gmail.com>
2017-01-24 22:07:02 +0100

    Add (default) pagination to dish type service

--- src/app/dish-type.service.ts
+++ src/app/dish-type.service.ts
@@ -1,5 +1,8 @@
 import { Injectable } from '@angular/core';
-import { Http } from '@angular/http';
+import { Http, Response, URLSearchParams } from '@angular/http';
+
+import { PaginationRequestInfo, PaginationRequestParams } from './models/pagination';
+
 import 'rxjs/Rx';
 
 @Injectable()
@@ -7,8 +10,19 @@ export class DishTypeService {
 
   constructor(private http : Http) {}
 
-  getDishTypes() {
-    return this.http.get('http://localhost:3000/api/v1/dishTypes')
+  getDishTypes(page ?: number) {
+    var params : URLSearchParams = new URLSearchParams();
+    var p : PaginationRequestParams;
+    if (page) {
+      p = PaginationRequestInfo.getPageRequestParams(page);
+    } else {
+      p = PaginationRequestInfo.getPageRequestParams(0);
+    }
+    params.set('offset', p.offset);
+    params.set('limit', p.limit);
+    return this.http.get('http://localhost:3000/api/v1/dishTypes', {
+      search : params
+    })
     .map((response) => response.json());
   }
 
---
+++ src/app/models/pagination.ts
@@ -0,0 +1,31 @@
+export interface PaginationResponseInfo {
+  elCount : number;
+  pageCount : number;
+  more : boolean;
+}
+
+export interface PaginationRequestParams {
+  offset : string;
+  limit : string;
+}
+
+export const PagingParams = {
+  pageElemNum : 10
+};
+
+export class PaginationRequestInfo {
+
+  static getPageRequestParams(page : number) : PaginationRequestParams {
+    if (page && page >= 0) {
+      return {
+        offset : (page * PagingParams.pageElemNum).toString(),
+        limit: (PagingParams.pageElemNum).toString()
+      }
+    }
+
+    return {
+      offset : '0',
+      limit : (PagingParams.pageElemNum).toString()
+    };
+  }
+}
