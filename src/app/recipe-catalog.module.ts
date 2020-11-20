0049ce1b07cd5a0820923861c9df3e15adaa3196
Riccardo Bellini <riccardo.bellini1988@gmail.com>
2020-11-20 17:23:38 +0100

    Add Angular Material

--- src/app/recipe-catalog.module.ts
+++ src/app/recipe-catalog.module.ts
@@ -14,6 +14,7 @@ import { SpinnerComponent } from './spinner/spinner.component';
 import { DishTypeService } from './dish-type.service';
 import { NoHrefDirective } from './no-href.directive';
 import { NewDishTypeComponent } from './new-dish-type/new-dish-type.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -30,7 +31,8 @@ import { NewDishTypeComponent } from './new-dish-type/new-dish-type.component';
     FormsModule,
     ReactiveFormsModule,
     HttpClientModule,
-    RoutingModule
+    RoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [
     DishTypeService
